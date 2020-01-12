ea7bcbb7d7442396f58d0abe036a59f8ebad61b5
zlim530 <zlim530@126.com>
2020-01-12 21:24:08 +0800

    dev

---
+++ C#/Bookstore/Bookstore.Client/BookPart2.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookstore.Client
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class Book
+    {
+        public string Report()
+        {
+            return $"#{ID} Name:{Name} Price:{Price}";
+        }
+    }
+}
---
+++ C#/Bookstore/Bookstore.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bookstore.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new BookstoreEntities();
+            var books = dbContext.Book;
+            foreach (var book in books)
+            {
+                //Console.WriteLine(book.Name);
+                Console.WriteLine(book.Report());
+            }
+        }
+    }
+}
