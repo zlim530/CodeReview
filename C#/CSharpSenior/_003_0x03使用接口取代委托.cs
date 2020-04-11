4bf2799f3c4faf6c881b30f92f67a295f4780b01
zlim530 <zlim530@126.com>
2020-04-12 00:26:27 +0800

    dev

---
+++ C#/CSharpSenior/_003_0x03使用接口取代委托.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpSenior {
+    class _003_0x03使用接口取代委托 {
+
+        static void Main(string[] args) {
+            IProductFactory pizzaFactory = new PizzaFactroy();
+
+            IProductFactory toyCarFactory = new ToyCarFactory();
+
+            var wrapFactory = new WrapFactory();
+
+            Box pizzaBox = wrapFactory.WrapProduct(pizzaFactory);
+            Box toyCarBox = wrapFactory.WrapProduct(toyCarFactory);
+
+            Console.WriteLine(pizzaBox.Product.Name);
+            Console.WriteLine(toyCarBox.Product.Name);
+        }
+    }
+
+    interface IProductFactory {
+        Product Make();
+    }
+
+    class PizzaFactroy : IProductFactory {
+        public Product Make() {
+            var product = new Product();
+            product.Name = "Pizza";
+            return  product;
+        }
+    }
+
+    class ToyCarFactory : IProductFactory {
+        public Product Make() {
+
+            var product = new Product();
+            product.Name = "ToyCar";
+            return product;
+        }
+    }
+
+    class Product {
+        public string Name { get; set; }
+    }
+
+    class Box {
+        public Product Product { get; set; }
+    }
+    class WrapFactory {
+
+        public Box WrapProduct(IProductFactory productFactory) {
+
+            var box = new Box();
+            Product product = productFactory.Make();
+            box.Product = product;
+            return box;
+        }
+    }
+
+}
