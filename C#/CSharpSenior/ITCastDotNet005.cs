f27b9271642f9c1bd230ab4d5e3d1e6a6d0e2ce9
zlim530 <zlim530@126.com>
2020-05-31 01:49:39 +0800

    C# daliy learning

--- C#/CSharpSenior/ITCastDotNet005.cs
+++ C#/CSharpSenior/ITCastDotNet005.cs
@@ -22,16 +22,166 @@ File.OpenWrite 方法快速创建文件流对象。
 byte[] 是任何数据的最根本表达形式，任何数据在计算机中进行存储最终都是二进制。
 FileStream 的 Position 属性为当前文件指针位置，每写一次就要移动 Position 以备下次读写时在正确的位置。Write
 用于向当前位置写入若干字节，Read 则用于在当前位置读取若干字节。
+使用 using 来简化操作：
+    注意：不是任何类型对象都可以写在 using() 的小括号中
+    只有实现了 IDispose 接口类型的对象才可以写，当 using{ ... } 执行完毕后会自动调用对象的 Dispose() 方法来释放资源
+
 
 */
 namespace 文件流操作 {
     public class Program {
-        static void Main(string[] args) {
+        /// <summary>
+        /// byte[] 与字符串之间的转换
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main0(string[] args) {
             string msg = "Hello,World!";
             // string 转换为 byte[] 数组
             byte[] bytes = System.Text.Encoding.UTF8.GetBytes(msg);
+            // 把 byte[] 数组转换为字符串
+            string msg2 = System.Text.Encoding.UTF8.GetString(bytes);
             Console.WriteLine("OK");
         }
+
+        // 流操作的都是字节，不能字节操作字符串。
+        
+        /// <summary>
+        /// 通过 FileStream 来写文件
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main1(string[] args){
+
+            // 注意：当我们使用相对路径来表示文件时，会默认在 CSharpSenior\bin\Debug\netcoreapp3.0 下进行读写操作
+            // 这里相对路径的访问位置对于不同的编译器是不同的，在 VSCode 中默认在当前项目 \CSharpSenior 下
+            // 而 VS 则默认在 \CSharpSenior\bin\Debug\netcoreapp3.0
+            // 创建文件流
+            FileStream fsWrite = new FileStream("first.txt",FileMode.Create,FileAccess.Write);
+            string msg = "今天是个好日子！";
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(msg);
+            fsWrite.Write(bytes,0,bytes.Length);
+            /* 
+            第一个参数：表示将指定的字节数组中的内容写入到文件中
+            第二个参数：表示参数1中字节数据的偏移量，一般为0，0表示从数组的最开始开始写入文件中
+            第三个参数：本次文件写入操作写入文件的实际字节个数
+            */
+
+            // 清空缓冲区；关闭文件流；释放资源
+            fsWrite.Flush();
+            // 清空缓冲区：因为程序是无权对进行磁盘文件的读写操作的，实际上程序是调用操作系统提供的API，因此当我们在程序
+            // 使用文件流进行读写操作时，实际上并不是实时的，而是操作系统也会有一个缓冲区，使用此方法就表示告诉操作系统
+            // 不要再等待缓冲区填满，而是现在就进行文件的读写操作
+            fsWrite.Close();
+            fsWrite.Dispose();
+            System.Console.WriteLine("OK");
+        }
+
+        /// <summary>
+        /// 使用 FileStream 来读文件
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main2(string[] args) {
+
+            // 使用 FileStream 文件流读取文本文件时不需要指定编码，因为编码是在 byte[] 数组转换为字符串时才需要指定的
+            // 而这里是直接读取 byte[] 数组中的字节数据，因此不需要使用编码
+            // using( ... ){ ... } 会自动调用相关资源的 Dispose() 方法
+            using (FileStream fsRead = new FileStream("first.txt",FileMode.Open,FileAccess.Read)) {
+                // 根据文件流的总字节数创建 byte[] 数组，这种方式会将文件中的内容一次性全部读取出来，并存入 byte[] 数组中
+                byte[] bytes = new byte[fsRead.Length];
+                fsRead.Read(bytes,0,bytes.Length);// 从流中读取字节块并将数据写入给定缓冲区中,count:最多读取的字节数。
+                /*
+                第一个参数：表示将指定文件流中读取到的内容写入 byte[] 数组中；
+                第二个参数：表示从文件流中读取数据的偏移量；
+                第三个参数：表示最多读取的字节数。
+                */
+                string msg = System.Text.Encoding.UTF8.GetString(bytes);
+                Console.WriteLine(msg);
+            }
+
+        }
+
+        /// <summary>
+        /// 测试文件拷贝 CopyFile 方法
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main3(string[] args) {
+            string source = @"C:\迅雷下载\致命魔术BD中英双字1280高清.rmvb";
+            string target = @"C:\Users\Lim\Desktop\test.rmvb";
+            CopyFile(source,target);
+            Console.WriteLine("OK");
+        }
+
+        /// <summary>
+        /// 通过文件流实现将 source 文件拷贝到 target :至少需要两个文件流对象
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        public static void CopyFile(string source,string target) {
+            // 创建一个读取源文件的文件流
+            using (FileStream fsRead = new FileStream(source,FileMode.Open,FileAccess.Read)) {
+                // 创建一个写入新文件的文件流
+                using (FileStream fsWrite = new FileStream(target,FileMode.Create,FileAccess.Write)) {
+                    // 拷贝文件时，创建一个缓冲区
+                    byte[] buffer = new byte[1024 * 1025 * 5];// 5MB
+                    // Read() 方法的返回值表示实际读取到字节数
+                    int len = fsRead.Read(buffer,0,buffer.Length);
+                    while (len > 0) {
+                        // 将读取到的内容写入到 target 文件中，第三个参数应该为实际读取到字节数而不是缓冲区数组的长度
+                        fsWrite.Write(buffer,0,len);
+                        /*
+                        如果想显式拷贝进度：
+                        double d = (fsWrite.Postion) / (double)fsRead.Length * 100;
+                        Console.WriteLine($"{d}");
+                        */
+                        len = fsRead.Read(buffer,0,buffer.Length);
+                    }
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// 测试文件加密 Encrypt 方法
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main4(string[] args) {
+            string source = @"C:\Users\Lim\Desktop\encrypt.jpg";
+            string target = @"C:\Users\Lim\Desktop\decrypt.jpg";
+            Encrypt(source,target);
+            Console.WriteLine("OK");
+        }
+
+        /// <summary>
+        /// 对文件进行加密操作并生成新的文件
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        public static void Encrypt(string source,string target) {
+            // 创建一个读取源文件的文件流
+            using (FileStream fsRead = new FileStream(source,FileMode.Open,FileAccess.Read)) {
+                // 创建一个写入新文件的文件流
+                using (FileStream fsWrite = new FileStream(target,FileMode.Create,FileAccess.Write)) {
+
+                    byte[] buffer = new byte[1024 * 4];// 4KB
+                    int count = 0;
+                    while ((count = fsRead.Read(buffer,0,buffer.Length)) > 0) {
+                        // 加密：实际就是按照一定的规则将缓冲区中读到的文件字节内容改变一下，然后再将改变完了字节内容写入新文件
+                        for (int i = 0; i < count; i++) {
+                            buffer[i] = (byte)((byte.MaxValue) - buffer[i]);// buffer[i] = 255 - buffer[i];
+                            // 或者用：buffer[i] = Convert.ToByte(255- buffer[i]);
+                        }
+                        // 写入新文件
+                        fsWrite.Write(buffer,0,count);
+                    }
+
+                }
+
+            }
+
+        }
+
+
+
     }
 }
 
