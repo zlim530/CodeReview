889adabd6c9191f2507b1970b6f53901abf6a629
zlim530 <zlim530@126.com>
2021-06-16 18:34:29 +0800

    6.16workday

--- C#/DailyWork/DailyCode/DailyLocalCode/SampleData.cs
+++ C#/DailyWork/DailyCode/DailyLocalCode/SampleData.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using static DailyLocalCode.SampleData;
 
 namespace DailyLocalCode
 {
     public class SampleData
     {
         public static IEnumerable<Defect> Defects { get; set; }
-        public static IEnumerable<Users> AllUsers { get; set; }
+        public static IEnumerable<User> AllUsers { get; set; }
         public static IEnumerable<Project> AllProjects { get; set; }
         public static IEnumerable<NotificationSubscription> AllSubscriptions { get; set; }
 
-        public class Users  
+        public static readonly DateTime Start = May(1);
+
+        public static readonly DateTime End = May(31);
+
+        public class User  
         {
             public string Name { get; set; }
 
@@ -22,23 +28,60 @@ namespace DailyLocalCode
         public class Project
         {
             public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("Project:{0}",Name);
+            }
+        }
+
+        public static DateTime May(int day)
+        {
+            return new DateTime(2013, 5, day);
         }
     }
 
     public class NotificationSubscription
     {
+        public Project Project { get; set; }
         public string EmailAddress { get; set; }
     }
 
     public class Defect
     {
-        public string Created { get; set; }
-        public int ID { get; set; }
-        public string LastModified { get; set; }
+        public Project Project { get; set; }
+        public User AssignedTo { get; set; }
         public string Summary { get; set; }
+        public Seveity Severity { get; set; }
+        public Status Status { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastModified { get; set; }
+        public User CreatedBy { get; set; }
+        public int ID { get; set; }
+
+        public Defect()
+        {
+            ID = StaticCounter.Next();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0,2}: {1}\r\n    ({2:d}-{3:d}, {4}/{5}, {6} -> {7})",ID,Summary,Created,LastModified, Severity,Status,CreatedBy.Name,AssignedTo == null ? "n/a":AssignedTo.Name);
+        }
+
     }
 
-    public enum Satus
+    public static class StaticCounter
+    {
+        static int next = 1;
+
+        public static int Next()
+        {
+            return next++;
+        }
+    }
+
+    public enum Status
     {
         Created,
         Accepted,
