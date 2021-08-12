15690fc1e1d2ff98365c43360df388843e33810f
zlim530 <zlim530@126.com>
2021-08-12 09:52:43 +0800

    CompleteRiderPipelineDemo

---
+++ C#/dotnet/PipelineDemo/PipelineDemo/PipelineStepLoopExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace PipelineDemo
+{
+    public static class PipelineStepLoopExtensions
+    {
+        public static IEnumerable<OUTPUT> Step<INPUT, OUTPUT>(this IEnumerable<INPUT> input,
+            IPipelineStep<INPUT, OUTPUT> step)
+        {
+            LoopStep<INPUT, OUTPUT> loopDecorator = new LoopStep<INPUT, OUTPUT>(step);
+            return loopDecorator.Process(input);
+        }
+    }
+}
--- C#/dotnet/PipelineDemo/PipelineDemo/Program.cs
+++ C#/dotnet/PipelineDemo/PipelineDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace PipelineDemo
@@ -16,7 +17,7 @@ namespace PipelineDemo
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
-            // double input = 1024.1024;
+            double input = 1024.1024;
 
             #region 基础实现
             // 基础实现
@@ -28,12 +29,12 @@ namespace PipelineDemo
             #region 依赖注入
             // 依赖注入
             // 需要安装 Microsoft.Extensions.DependencyInjection
-            var services = new ServiceCollection();
-            services.AddTransient<TrivalPipeline>();
-            var provider = services.BuildServiceProvider();
-            var trival = provider.GetService<TrivalPipeline>();
-            string result = trival.Process(input);
-            Console.WriteLine(result);
+            // var services = new ServiceCollection();
+            // services.AddTransient<TrivalPipeline>();
+            // var provider = services.BuildServiceProvider();
+            // var trival = provider.GetService<TrivalPipeline>();
+            // string result = trival.Process(input);
+            // Console.WriteLine(result);
             #endregion
 
             #region 条件式组装
@@ -67,9 +68,13 @@ namespace PipelineDemo
             // 可迭代执行是指当我们的管道中注册了多个功能模块时，不是一次性执行完所有的功能模块，而是每次只执行一个
             // 功能，后续功能会在下次执行该管道对应的代码块时接着执行，直到该管道中所有的功能模块执行完毕为止。
             // 该特性主要是通过 yield return 来实现
-            
+            var list = Enumerable.Range(0, 10);
+            foreach (var item in list.Step(new DoubleStep()))
+            {
+                Console.WriteLine(item);
+            }
             #endregion
-            
+
         }
     }
 }
