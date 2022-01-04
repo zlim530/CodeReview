ac0a9970378c2020d210d10c6cfb1c90e654d1c1
zlim530 <zlim530@126.com>
2022-01-04 16:28:28 +0800

    addZipTwoListsriderproject

---
+++ C#/dotnet/net5.0/ZipTwoLists/ZipTwoLists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Node
+{
+    public int val;
+    public Node next;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var list1 = MakeList(1, 3, 5, 7, 7, 9);
+        var list2 = MakeList(2, 6, 7, 8);
+        PrintList(list1);
+        PrintList(list2);
+    }
+
+    static Node MakeList(params int[] a)
+    {
+        Node head = null;
+        for (int i = a.Length - 1; i >= 0; i--)
+            head = new Node{val = a[i], next = head};
+        return head;
+    }
+
+    static void PrintList(Node list)
+    {
+        while (list != null)
+        {
+            Console.Write($"{list.val}->");
+            list = list.next;
+        }
+
+        Console.WriteLine("null");
+    }
+}
