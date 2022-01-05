6894a2dcb31cd06e26866296dc0b12a1613ffd26
zlim530 <zlim530@126.com>
2022-01-05 16:21:01 +0800

    finishZipTwoLists

--- C#/dotnet/net5.0/ZipTwoLists/ZipTwoLists/Program.cs
+++ C#/dotnet/net5.0/ZipTwoLists/ZipTwoLists/Program.cs
@@ -14,6 +14,62 @@ class Program
         var list2 = MakeList(2, 6, 7, 8);
         PrintList(list1);
         PrintList(list2);
+        var result = Zip(list1, list2);
+        PrintList(result);
+    }
+
+    static Node Zip(Node p, Node q)
+    {
+        Node head = new Node(), zipper = head; // dummy head
+        while (p != null && q != null)
+        {
+            if (p.val <= q.val)
+            {
+                zipper.next = p;
+                p = p.next;
+            }
+            else
+            {
+                zipper.next = q;
+                q = q.next;
+            }
+
+            zipper = zipper.next;
+        }
+
+        zipper.next = p == null ? q : p;
+        return head.next;
+
+        /*while (true)
+        {
+            if (p == null)
+            {
+                zipper.next = q;
+                break;
+            }
+
+            if (q == null)
+            {
+                zipper.next = p;
+                break;
+            }
+            
+            if (p.val <= q.val)
+            {
+                zipper.next = p;
+                p = p.next;
+            }
+            else
+            {
+                zipper.next = q;
+                q = q.next;
+            }
+
+            zipper = zipper.next;
+            
+        }
+
+        return head.next;*/
     }
 
     static Node MakeList(params int[] a)
