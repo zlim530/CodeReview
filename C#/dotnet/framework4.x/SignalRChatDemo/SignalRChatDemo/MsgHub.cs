782520b90c1c723120b149da07c99e8123e331a3
zlim530 <zlim530@126.com>
2022-01-11 16:26:33 +0800

    addSignalRChatDemo

---
+++ C#/dotnet/framework4.x/SignalRChatDemo/SignalRChatDemo/MsgHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChatDemo
+{
+    public class MsgHub : Hub
+    {
+        // 在 hub 中编写的方法，都是要被客户端调用的方法
+        public void SendMsg(string txt)
+        {
+            // 服务器主动调用客户端的方法
+            Clients.All.getMsg(txt);
+        }
+    }
+}
