ae9bf469c9ca2963307efae8cbd029b0a44fce83
zlim530 <zlim530@126.com>
2022-01-29 14:25:15 +0800

    modifycomments

--- C#/dotnet/net5.0/Reflection/Reflection/Program.cs
+++ C#/dotnet/net5.0/Reflection/Reflection/Program.cs
@@ -4,7 +4,13 @@ using System.Reflection;
 namespace Reflection
 {
     /// <summary>
-    /// 反射：就是操作 DLL 文件的一个类库
+    /// exe/dll 的主要区别是 exe 文件有入口 Main 方法：即 dll 文件也可以有 Main 方法但不是必须的，而 exe 文件必须有 Main 入口方法
+    /// metadata：元数据：描述 exe/dll 文件的一个数据清单，反射（Reflection）用来操作获取元数据
+    /// 【1】更新程序时（更新自己的 DLL，可直接替换）
+    /// 【2】使用别人的 DLL 文件：可以读取私有的东西
+    /// 反射：就是操作 DLL 文件的一个类库或者就是一个操作 metadata 的类库：可以把反射当成一个小工具用来读取或者操作元数据的
+    /// 反射可以获取类、方法、特性、属性字段，为什么要通过反射间接去操作？1.因为我们需要动态获取；2.可以读取私有的对象
+    /// 哪些地方使用了反射：ASP .Net MVC、ORM、LOC、AOP 几乎所有的框架都会使用反射
     /// 怎么使用：1--查找 DLL 文件; 2--通过 Reflection 反射类库里的各种方法来操作 DLL 文件
     /// </summary>
     class Program
