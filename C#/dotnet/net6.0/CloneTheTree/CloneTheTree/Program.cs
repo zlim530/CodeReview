e61411d95902cdb10352ecc582c8e52434d475bb
zlim530 <zlim530@126.com>
2022-01-30 12:46:03 +0800

    下载了net6.0和vs2022并完成了CloneTheTree

---
+++ C#/dotnet/net6.0/CloneTheTree/CloneTheTree/Program.cs
@@ -0,0 +1,37 @@
+﻿using CloneTheTree;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] a = { 1, 2, 3, 4, 5, 6, 7};
+var tree = Utilites.BuildTree(a, 0, a.Length - 1);
+var newTree = Clone(tree);
+Utilites.DFS(newTree);
+
+static Node Clone(Node root)
+{ 
+    var queue = new Queue<Node>();
+    var newRoot = new Node(root.val);
+    queue.Enqueue(root);
+    queue.Enqueue(newRoot);
+    while (queue.Count > 0) // 即 queue 不为空的时候
+    { 
+        var original = queue.Dequeue();
+        var cloned = queue.Dequeue();
+        if (original.left != null)
+        {
+            cloned.left = new Node(original.left.val);
+            queue.Enqueue(original.left);
+            queue.Enqueue(cloned.left);
+        }
+        
+        if (original.right != null)
+        {
+            cloned.right = new Node(original.right.val);
+            queue.Enqueue(original.right);
+            queue.Enqueue(cloned.right);
+        }
+
+    }
+
+    return newRoot;
+}
---
+++ C#/dotnet/net6.0/CloneTheTree/CloneTheTree/Tree.cs
@@ -0,0 +1,45 @@
+﻿namespace CloneTheTree;
+
+class Node
+{
+    public int val;
+    public Node left;
+    public Node right;
+    public Node(int val)
+    {
+        this.val = val;
+    }
+}
+
+static class Utilites
+{
+    /// <summary>
+    /// 把一个数组转换为一个平衡的二叉树
+    /// </summary>
+    /// <param name="a">待转换的数组</param>
+    /// <param name="li">左边边界索引</param>
+    /// <param name="hi">右边边界索引</param>
+    /// <returns></returns>
+    public static Node BuildTree(int[] a, int li, int hi)
+    {
+        if (hi < li) return null;
+        int mi = li + (hi - li) / 2;
+        var node = new Node(a[mi]);
+        node.left = BuildTree(a, li, mi - 1);
+        node.right = BuildTree(a,mi + 1, hi);
+        return node;
+    }
+
+    /// <summary>
+    /// 中序的 DFS 遍历
+    /// </summary>
+    /// <param name="root">根节点</param>
+    public static void DFS(Node root)
+    {
+        if (root == null) return;
+        DFS(root.left);
+        Console.WriteLine(root.val);
+        DFS(root.right);
+    }
+
+}
