a98c248a0e1a83ccda1af88b301d8efcead21da5
zlim530 <zlim530@126.com>
2022-03-13 18:48:33 +0800

    完成了JQuery表单验证示例代码的实操

--- 前端相关/jQuery/jQuery表单验证-jQuery实战/.idea/17.x/js/main.js
+++ 前端相关/jQuery/jQuery表单验证-jQuery实战/.idea/17.x/js/main.js
@@ -1,18 +1,31 @@
-// var validator = {};
-// validator.validate_min = function (val,rule) {
-// }
-//
-// validator.validate_maxlength = function (val,rule) {
-// }
+$(function () {
+    'use strict';
 
+    /*选择页面中所有的input[data-rule]即所有input框中所有自定义了data-rule属性的input框*/
+    var $inputs = $('[data-rule]')
+        , $form = $('#signup')
+        , inputs = [];
 
+    $inputs.each(function (index,node) {
+        /*解析每一个input的验证规则*/
+        var tmp = new Input(node);
+        inputs.push(tmp);
+    })
+    
+    $form.on('submit', function (e) {
+        e.preventDefault();
+        /* 通过jQuery选中所有的input框并一起进行blur事件的触发：这是为了显示错误信息用的：如果有错误提示的话 */
+        $inputs.trigger('blur');
+        for (var i = 0; i < inputs.length; i ++){
+            var item = inputs[i];
+            var r = item.validator.is_valid();
+            if (!r) {
+                alert('invalid');
+                return;
+            }
+        }
 
-/*选择页面中所有的input[data-rule]即所有input框中所有自定义了data-rule属性的input框*/
+        alert('注册成功');
+    });
 
-
-/*解析每一个input的验证规则*/
-
-
-/*验证*/
-var name_validator = new Validator(user_input, rule);
-name_validator.is_valid();
+});
