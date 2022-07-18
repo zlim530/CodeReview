cb9bc3311b9b281ec7f6aa3c66e7956213956e36
zlim530 <zlim530@126.com>
2022-07-18 16:53:58 +0800

    异步其他问题

--- C#/dotnet/net6.0/DailyTest/DailyTest/Program.cs
+++ C#/dotnet/net6.0/DailyTest/DailyTest/Program.cs
@@ -2,6 +2,11 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// async 和 awiat 基本使用
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         static async Task Main0(string[] args)
         {
             Console.WriteLine("Hello, World!");
@@ -18,6 +23,11 @@
             Console.WriteLine("File Content: " + s);
         }
 
+        /// <summary>
+        /// WhenAll
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         static async Task Main1(string[] args)
         {
             string[] files = Directory.GetFiles(@"D:\temp");
@@ -34,12 +44,36 @@
             
         }
 
-        static async Task Main(string[] args)
+        /// <summary>
+        /// CancellationToken
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static async Task Main2(string[] args)
         {
             CancellationTokenSource cts = new CancellationTokenSource();
-            cts.CancelAfter(5000);
+            //cts.CancelAfter(5000);
             CancellationToken token = cts.Token;
-            await DownloadHtmlAsync("https://www.baidu.com",100, token);
+            //await DownloadHtmlAsync("https://www.baidu.com",100, token);
+            DownloadHtmlAsync("https://www.baidu.com",200, token);
+            while (Console.ReadLine() != "e")
+            {
+
+            }
+            cts.Cancel();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 异步其他问题：yield return
+        /// </summary>
+        /// <param name="args"></param>
+        static async Task Main(string[] args)
+        {
+            await foreach (var item in Test3())
+            {
+                Console.WriteLine(item);
+            }
         }
 
         static async Task<int> ReadCharsCount(string fileName)
@@ -48,24 +82,81 @@
             return s.Length;
         }
 
-
+        /// <summary>
+        /// 一般不需要自己处理 CancellationToken，只要做到“能转发 CancellationToken 就转发”即可
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="n"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         static async Task DownloadHtmlAsync(string url, int n, CancellationToken cancellationToken)
         {
             using (HttpClient client = new HttpClient())
             {
                 for (int i = 0; i < n; i++)
                 {
-                    string html = await client.GetStringAsync(url);
+                    //var resp = await client.GetAsync(url, cancellationToken);
+                    var resp = await client.GetAsync(url);
+                    var html = await resp.Content.ReadAsStringAsync();
+                    //string html = await client.GetStringAsync(url);
                     Console.WriteLine($"{DateTime.Now} : {html}");
-                    /*if (cancellationToken.IsCancellationRequested)
+                    if (cancellationToken.IsCancellationRequested)
                     {
                         Console.WriteLine("The request is cancelled.");
                         break;
-                    }*/
-                    cancellationToken.ThrowIfCancellationRequested();
+                    }
+                    //cancellationToken.ThrowIfCancellationRequested();
                 }
             }
         }
         
+        /// <summary>
+        /// 常规的写法是一次性组装完所有数据后返回，然后在 Main 函数中打印出来
+        /// </summary>
+        /// <returns></returns>
+        static IEnumerable<string> Test1()
+        {
+            List<string> list = new List<string>();
+            list.Add("hello");
+            list.Add("async");
+            list.Add("async.com");
+            return list;
+        }
+
+        /// <summary>
+        /// 而使用 yield return 调试后可以发现它是先 return hello, 在 Main 函数中进行打印，然后又进入方法返回 async 在屏幕上打印，是一次性一次性的将数据返回而不用等所有数据组装完毕后再返回；
+        /// yield return 背后的原理是编译器将其编译为状态机，这与 async await 的语法糖类似，因此在 C# 8.0 之前 async 方法中不能用 yield
+        /// </summary>
+        /// <returns></returns>
+        static IEnumerable<string> Test2()
+        {
+            yield return "hello";
+            yield return "async";
+            yield return "async.com";
+        }
+
+        /// <summary>
+        /// 错误写法：编译不通过
+        /// Task<IEnumerable<string>>”不是迭代器接口类型，因此“Program.Test3()”体不能是迭代器块 
+        /// </summary>
+        /// <returns></returns>
+        /*static async Task<IEnumerable<string>> Test3()
+        {
+            yield return "hello";
+            yield return "async";
+            yield return "async.com";
+        }*/
+
+        /// <summary>
+        /// 正确写法：编译不通过
+        /// 从 C# 8.0 开始，把返回值声明为 IAsyncEnumerable(不要带 Task)，然后遍历时用 await foreach() 即可
+        /// </summary>
+        /// <returns></returns>
+        static async IAsyncEnumerable<string> Test3()
+        {
+            yield return "hello";
+            yield return "async";
+            yield return "async.com";
+        }
     }
 }
---
+++ C#/dotnet/net6.0/DailyTest/aspnetcoreCancellationToken/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using aspnetcoreCancellationToken.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace aspnetcoreCancellationToken.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            // 不需要管这个参数怎么来的，这是 ASP .NET Core 框架内部处理的，我们只负责转发即可
+            // 当用户访问别的网站时此时就会停止此方法的调用
+            await DownloadHtmlAsync("https://www.baidu.com", 100 ,cancellationToken);
+            // 如果我们不转发 CancellationToken 参数，则当用户已经离开此程序的服务站点，但此方法仍在执行着
+            //await DownloadHtmlAsync("https://www.baidu.com", 100 ,CancellationToken.None);
+            return View();
+        }
+
+        static async Task DownloadHtmlAsync(string url, int n, CancellationToken cancellationToken)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    var resp = await client.GetAsync(url, cancellationToken);
+                    var html = await resp.Content.ReadAsStringAsync();
+                    Debug.WriteLine(html);
+                }
+            }
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
