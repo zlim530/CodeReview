462ddb32382fb1656f8248f07c03eb894284e8c0
zlim530 <zlim530@126.com>
2023-03-08 17:52:57 +0800

    first try OpenAI.GPT3 with C#

---
+++ C#/OpenAI/ChatGPTWithCSharp/ChatGPTWithCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var api = new OpenAI_API.OpenAIAPI("YOUR_API_KEY");
+var question = Console.ReadLine();
+var result = await api.Completions.GetCompletion(question);
+Console.WriteLine(result);
