7c9ab1e9cb25c693b8b15f98e2392fee2238ef57
zlim530 <zlim530@126.com>
2023-05-09 22:41:22 +0800

    async & await

--- DotNet/CodeCheck/CodeCheck/003-ConcurrencyProgram.cs
+++ DotNet/CodeCheck/CodeCheck/003-ConcurrencyProgram.cs
@@ -3,7 +3,7 @@ using System;
 
 public class _003_ConcurrencyProgram
 {
-    public static void Main00(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Main-Begin");
 
@@ -12,18 +12,20 @@ public class _003_ConcurrencyProgram
         //UseThreadPool();
         //UseTask();
         //UseTaskCPU();
-        UseTaskWait();
+        //UseTaskWait();
+        UseTaskWhenXXX();
         //UseTaskCPU();
-        UseAsyncAwait();
+        //UseAsyncAwait();
         //UseLinq();
         //UsePLinq();
 
         Console.WriteLine("Main-End");
 
-        while (true) { Thread.Sleep(1000); }
+        //while (true) { Thread.Sleep(1000); }
+        Console.ReadLine();
     }
 
-    public static async Task Main()
+    public static async Task Main00()
     {
         Console.WriteLine("Main-Begin");
         await UseAsyncAwaitAsync();
@@ -43,6 +45,19 @@ public class _003_ConcurrencyProgram
         Console.WriteLine($"{id} End");
     }
 
+    public static string Work(string id)
+    {
+        Console.WriteLine($"{id} Begin");
+        char[] chars = new char[4] { 'A', 'B', 'C', 'D' };
+        foreach (var item in chars)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"{id} {item}");
+        }
+        Console.WriteLine($"{id} End");
+        return id;
+    }
+
     //优点：代码简单
     //缺点：串行执行，相互阻塞
     public static void Standard()
@@ -53,7 +68,7 @@ public class _003_ConcurrencyProgram
 
     //线程：创建和控制线程，设置其优先级并获取其状态。
     //优点：可以并行
-    //缺点：开销大
+    //缺点：开销大：不同线程上下文的切换等消耗大
     public static void UseThread()
     {
         Thread t1 = new Thread(x =>
@@ -70,7 +85,7 @@ public class _003_ConcurrencyProgram
 
     #region 线程
 
-    //线程池：把线程托管到.NET线程池中，减少了线程创建的开销
+    //线程池：把线程托管到.NET线程池中，减少了线程创建（与上下文切换）的开销
     //优点：线程开销减少
     public static void UseThreadPool()
     {
@@ -105,7 +120,7 @@ public class _003_ConcurrencyProgram
         });
     }
 
-    //优点：默认根据CPU数量开启线程数量,
+    //优点：Task.Run 默认根据CPU数量开启线程数量,
     public static void UseTaskCPU()
     {
         Console.Read();
@@ -143,6 +158,38 @@ public class _003_ConcurrencyProgram
 
         Task.WaitAll(t1, t2);// 等待这两个线程，等这两个 task 执行完成后再执行 UseTaskWait 方法后面的代码：相当于将异步操作变成同步执行，可以手动控制异步代码的执行顺序
         // 在本代码中实现的效果是：Main 方法中打印的 Main-Begin 和 Main-End 会把 Work 方法中打印的值包起来
+    }
+
+    public static async void UseTaskWhenXXX()
+    {
+        var t1 = Task.Run(() =>
+        {
+            Work(1);
+        });
+
+        var t2 = Task.Run(() =>
+        {
+            Work(2);
+            Work(3);
+        });
+
+        //var t3 = Task.Run(() => {
+        //    return Work("4");
+        //});
+        //var t4 = Task.Run(() => {
+        //    return  Work("5");
+        //});
+        //await Task.WhenAll(t1, t2);// 等待所有任务完成。
+        //var completedTasks = await Task.WhenAll<string>(t3, t4); 注意点：1.当所有任务返回值结果类型一致时，返回的对象是含有所有结果的 array 对象
+        //await Console.Out.WriteLineAsync(string.Join(",",completedTasks));
+        
+        //await Task.WhenAny(t1, t2);
+        var CompletedTask = await Task.WhenAny(t1, t2);
+        //Console.WriteLine(CompletedTask);
+        if (CompletedTask == t1)
+        {
+            Console.WriteLine("CompletedTask is t1");// WhenAny 并不会终止未完成任务的执行，只是不会再等待所有的任务完成后再执行后续的代码，而是只要有任意一个任务完成了就会接着执行 await 后面的代码
+        }
     }
 
     //async/await：异步方法同步执行：相当于 Task.WaitXXX() 方法执行的操作，简化写 Task.WaitXXX() 的代码，只需要使用 async/await 关键字即可
